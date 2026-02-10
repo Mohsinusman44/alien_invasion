aecd96e944664c60761323b6403c50aaf59ff8ca
musma35 <musma35@wgu.edu>
2026-02-10 15:05:45 -0500

    ship left movement added.

--- alien_invasion.py
+++ alien_invasion.py
@@ -26,17 +26,25 @@ class AlienInvasion:
             
             self._update_screen()
             
+            self.ship.update()
+            
             self.clock.tick(60)
 
     def _check_events(self):
         """Respond to keypresses and mouse events."""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                sys.exit() 
+                sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    #move ths ship to the right.
-                    self.ship.rect.x += 1
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
             
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
