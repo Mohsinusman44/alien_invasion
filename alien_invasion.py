7f67f47aab2ef71846e78034bff9763b391d7eb6
musma35 <musma35@wgu.edu>
2026-02-13 20:25:41 -0500

    added settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,8 +10,9 @@ class AlienInvasion:
         """Initialize the game, and create game resources."""
         pygame.init()
         self.settings = Settings()
-        
-        #self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.setting 12=
+        #comment
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
 
