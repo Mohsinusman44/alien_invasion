b999662e72c72e6572827cbd2a92e37e3831dd33
musma35 <musma35@wgu.edu>
2026-02-13 20:07:14 -0500

    Reference point to switch on PC

--- settings.py
+++ settings.py
@@ -6,8 +6,8 @@ class Settings:
 
         #Screen settings
         self.screen_width = 1200
-        self.screen_height = 600
-        self.bg_color = (255, 0, 0)
+        self.screen_height = 800
+        self.bg_color = (0, 0, 0)
         
         #Ship Settings
         self.ship_speed = 1.5
